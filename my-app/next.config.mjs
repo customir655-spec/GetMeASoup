7fc236802523c58389e9279966c02c7cc2c16ffb
customir655-spec <customir655@gmail.com>
2026-05-04 23:24:56 +0530

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -4,10 +4,7 @@ typescript: {
     // This allows the build to continue even with the 'baseUrl' warning
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Recommended to skip this during emergency deploys too
-    ignoreDuringBuilds: true,
-  },
+ 
   /* config options here */
 };
 
